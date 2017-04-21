fe1e54e45156fa28fcaa18c033c23ada20e4336c
Ameen5719 <naughtyameen5719@gmail.com>
2017-04-21 12:20:53 -0700

    0

--- js/script.js
+++ js/script.js
@@ -1,96 +1,10 @@
 $(function () {
-    $("section#about-me").slideDown("fast");
-    $("a#skill-nav").click(function () {
-        $("section#about-me, section#contact-me").slideUp("fast");
-        $("section#skill").slideDown("swing");
-
+    $("#sidenav-toggle").click(function(){
+        $("#sidenav").css("width", "40%");
+        $("#sidenav-toggle").fadeOut("fast");
     });
-    $("a#contact-nav").click(function () {
-        $("section#about-me, section#skill").slideUp("fast");
-        $("section#contact-me").slideDown("swing");
+    $("#sidenav-collapse").click(function(){
+        $("#sidenav").css("width", "0%");
+        $("#sidenav-toggle").fadeIn("fast");
     });
-    $("a#about-nav").click(function () {
-        $("section#skill, section#contact-me").slideUp("fast");
-        $("section#about-me").slideDown("swing");
-
-    });
-});
-var ctx = document.getElementById("HTML_5").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ["HTML 5"],
-        datasets: [{
-            backgroundColor: [
-        "#2ecc71",
-        "#95a5a6",
-              ],
-            data: [80, 20]
-    }]
-    }
-});
-
-
-var ctx = document.getElementById("CSS_3").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ["CSS 3"],
-        datasets: [{
-            backgroundColor: [
-        "#3498db",
-        "#95a5a6",
-      ],
-            data: [75, 25]
-    }]
-    }
-});
-
-
-var ctx = document.getElementById("JS").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ["JavaScript"],
-        datasets: [{
-            backgroundColor: [
-        "#9b59b6",
-        "#95a5a6",
-            ],
-            data: [90, 10]
-    }]
-    }
-});
-
-var ctx = document.getElementById("BTS").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ["BOOTSTRAP 3"],
-        datasets: [{
-            backgroundColor: [
-        "#34495e",
-        "#95a5a6",
-              ],
-            data: [95, 5]
-    }]
-    }
-});
-
-
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'polarArea',
-    data: {
-        labels: ["HTML 5", "CSS 3", "JavaScript", "Bootstrap 3", ],
-        datasets: [{
-            backgroundColor: [
-        "#9b59b6",
-        "#3498db",
-        "#34495e",
-        "#34495e"
-      ],
-            data: [80, 75, 90, 95]
-    }]
-    }
 });
