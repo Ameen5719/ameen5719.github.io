ab3f2308a37f1e97bb35329fb9c419748dc10304
Ameen5719 <naughtyameen5719@gmail.com>
2017-04-22 00:19:35 -0700

    5

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 $(function () {
-    $("#sidenav-toggle").click(function(){
-        $("#sidenav").css("width", "40%");
-        $("#sidenav-toggle").fadeOut("fast");
+    $("section#about-me").slideDown("fast");
+    $("a#skill-nav").click(function () {
+        $("section#about-me, section#contact-me").slideUp("fast");
+        $("section#skill").slideDown("swing");
+
     });
-    $("#sidenav-collapse").click(function(){
-        $("#sidenav").css("width", "0%");
-        $("#sidenav-toggle").fadeIn("fast");
+    $("a#contact-nav").click(function () {
+        $("section#about-me, section#skill").slideUp("fast");
+        $("section#contact-me").slideDown("swing");
     });
+    $("a#about-nav").click(function () {
+        $("section#skill, section#contact-me").slideUp("fast");
+        $("section#about-me").slideDown("swing");
+
+    });
+    $("#sidenav-trigger").click(function(){
+        $(".sidenav").css("width", "30%");
+    });
+    $("#sidsidenav-close").click(function() {
+        $(".sidenav").css("width", "0");
+    })
 });
