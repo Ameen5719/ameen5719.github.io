44d3a7171cb1efe726bfbd78608b9e5a230be022
Ameen5719 <naughtyameen5719@gmail.com>
2017-04-24 10:09:32 -0700

    9

--- js/script.js
+++ js/script.js
@@ -12,117 +12,157 @@ $(document).ready(function () {
         $(".gallery-filter li").removeClass("active");
         $(this).closest("li").addClass("active");
     });
-            var ctx = $("#html5");
-        var html5 = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: ["HTML 5"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [90,10],
+    var ctx = $("#polarchart");
+    var polarchart = new Chart(ctx, {
+        type: 'polarArea',
+        data: data = {
+            labels: [
+        "HTML 5",
+"CSS 3",
+"BOOTSTRAP",
+"JAVASCRIPT",
+"JQUERY",
+"ANGULARJS",
+    ],
+            datasets: [
+                {
+                    data: [90,
+75,
+90,
+90,
+75,
+10],
                     backgroundColor: [
+'rgba(255, 206, 86, 0.5)',
+'rgba(54, 162, 235, 0.5)',
+
+'rgba(75, 192, 192, 0.5)',
+'rgba(255, 159, 64, 0.5)',
+'rgba(153, 102, 255, 0.5)',
+ 'rgba(255, 99, 132, 0.5)'
+            ],
+                    hoverBackgroundColor: ['rgba(255, 206, 86, 1)',
+'rgba(54, 162, 235, 1)',
+
+'rgba(75, 192, 192, 1)',
+'rgba(255, 159, 64, 1)',
+'rgba(153, 102, 255, 1)',
+ 'rgba(255, 99, 132, 1)'  ]
+        }]
+        },
+
+    });
+    var ctx = $("#html5");
+    var html5 = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["HTML 5"],
+            datasets: [{
+                label: '# of Votes',
+                data: [90, 10],
+                backgroundColor: [
                 'rgba(255, 206, 86, 0.5)'
             ],
-                    borderColor: [
+                borderColor: [
                 'rgba(255, 206, 86, 1)'
             ],
-                    borderWidth: 1
+                borderWidth: 1
         }]
-            },
+        },
 
-        });
-        var ctx = $("#css3");
-        var css3 = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: ["CSS 3"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [75,25],
-                    backgroundColor: [
+    });
+    var ctx = $("#css3");
+    var css3 = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["CSS 3"],
+            datasets: [{
+                label: '# of Votes',
+                data: [75, 25],
+                backgroundColor: [
                 'rgba(54, 162, 235, 0.5)'
             ],
-                    borderColor: [
+                borderColor: [
                 'rgba(54, 162, 235, 1)'
             ],
-                    borderWidth: 1
+                borderWidth: 1
         }]
-            },
+        },
 
-        });
-        var ctx = $("#bootstrap3");
-        var bootstrap3 = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: ["BOOTSTRAP"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [90,10],
-                    backgroundColor: [
+    });
+    var ctx = $("#bootstrap3");
+    var bootstrap3 = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["BOOTSTRAP"],
+            datasets: [{
+                label: '# of Votes',
+                data: [90, 10],
+                backgroundColor: [
                 'rgba(75, 192, 192, 0.5)'
             ],
-                    borderColor: [
+                borderColor: [
                 'rgba(75, 192, 192, 1)'
             ],
-                    borderWidth: 1
+                borderWidth: 1
         }]
-            },
+        },
 
-        });
-        var ctx = $("#javascript");
-        var javascript = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: ["JAVASCRIPT"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [90, 10],
-                    backgroundColor: [
+    });
+    var ctx = $("#javascript");
+    var javascript = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["JAVASCRIPT"],
+            datasets: [{
+                label: '# of Votes',
+                data: [90, 10],
+                backgroundColor: [
                 'rgba(255, 159, 64, 0.5)'
             ],
-                    borderColor: [
+                borderColor: [
                 'rgba(255, 159, 64, 1)'],
-                    borderWidth: 1
+                borderWidth: 1
         }]
-            },
+        },
 
-        });
-        var ctx = $("#jquery");
-        var jquery = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: ["JQUERY"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [75, 25],
-                    backgroundColor: [
+    });
+    var ctx = $("#jquery");
+    var jquery = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["JQUERY"],
+            datasets: [{
+                label: '# of Votes',
+                data: [75, 25],
+                backgroundColor: [
                 'rgba(153, 102, 255, 0.5)'
             ],
-                    borderColor: [
-                'rgba(153, 102, 255, 1)' ],
-                    borderWidth: 1
+                borderColor: [
+                'rgba(153, 102, 255, 1)'],
+                borderWidth: 1
         }]
-            },
+        },
 
-        });
-        var ctx = $("#angularjs");
-        var angular = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: ["ANGULARJS"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [10,90],
-                    backgroundColor: [
+    });
+    var ctx = $("#angularjs");
+    var angular = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["ANGULARJS"],
+            datasets: [{
+                label: '# of Votes',
+                data: [10, 90],
+                backgroundColor: [
                 'rgba(255, 99, 132, 0.5)'
             ],
-                    borderColor: [
+                borderColor: [
                 'rgba(255,99,132,1)'
             ],
-                    borderWidth: 1
+                borderWidth: 1
         }]
-            },
+        },
 
-        });
+    });
 
 });
