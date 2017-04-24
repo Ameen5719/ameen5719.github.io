8552148a4c7bd2b524d7a8b87cb49293a942a86d
Ameen5719 <naughtyameen5719@gmail.com>
2017-04-24 11:22:56 -0700

    9

--- js/script.js
+++ js/script.js
@@ -12,43 +12,39 @@ $(document).ready(function () {
         $(".gallery-filter li").removeClass("active");
         $(this).closest("li").addClass("active");
     });
-    var ctx = $("#polarchart");
+});
+    var ctx = "polarchart";
     var polarchart = new Chart(ctx, {
-        type: 'polarArea',
-        data: data = {
-            labels: [
-        "HTML 5",
-"CSS 3",
-"BOOTSTRAP",
-"JAVASCRIPT",
-"JQUERY",
-"ANGULARJS",
-    ],
-            datasets: [
-                {
-                    data: [90,
-75,
-90,
-90,
-75,
-10],
-                    backgroundColor: [
-'rgba(255, 206, 86, 0.5)',
-'rgba(54, 162, 235, 0.5)',
+    type: 'polarArea',
+    data: {
 
-'rgba(75, 192, 192, 0.5)',
-'rgba(255, 159, 64, 0.5)',
-'rgba(153, 102, 255, 0.5)',
- 'rgba(255, 99, 132, 0.5)'
-            ],
-                    hoverBackgroundColor: ['rgba(255, 206, 86, 1)',
-'rgba(54, 162, 235, 1)',
 
-'rgba(75, 192, 192, 1)',
-'rgba(255, 159, 64, 1)',
-'rgba(153, 102, 255, 1)',
- 'rgba(255, 99, 132, 1)'  ]
+        labels: [
+                    "HTML 5","CSS 3","BOOTSTRAP","JAVASCRIPT","JQUERY","ANGULARJS"
+                    ],
+        datasets: [{
+            data: [90,
+                                       75,
+                                       90,
+                                       90,
+                                       75,
+                                       10],
+            backgroundColor: ['rgba(255, 206, 86, 0.5)',
+                                                    'rgba(54, 162, 235, 0.5)',
+                                                    'rgba(75, 192, 192, 0.5)',
+                                                    'rgba(255, 159, 64, 0.5)',
+                                                    'rgba(153, 102, 255, 0.5)',
+                                                     'rgba(255, 99, 132, 0.5)'
+                                                ],
+            hoverBackgroundColor: ['rgba(255, 206, 86, 1)',
+                                            'rgba(54, 162, 235, 1)',
+                                            'rgba(75, 192, 192, 1)',
+                                            'rgba(255, 159, 64, 1)',
+                                            'rgba(153, 102, 255, 1)',
+                                             'rgba(255, 99, 132, 1)'
+                                        ]
         }]
-        },
 
-    });
+        }
+                   });
+             /**/
